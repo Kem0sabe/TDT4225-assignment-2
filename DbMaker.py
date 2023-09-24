2e66a5dd6a1c1a604583d4119f85f0f401efbfcf
Martin V. Larsen <martivl@stud.ntnu.no>
2023-09-24 14:48:42 +0200

    add last queries, fix syntax, comment code

--- DbMaker.py
+++ DbMaker.py
@@ -1,93 +1,117 @@
+
 import os
 from datetime import datetime
 import mysql.connector as mysql
 
-
-class DbMaker:
+class DbMaker():
     DATASET_PATH = "dataset/"
 
-    def __init__(self, HOST="localhost", DATABASE="db", USER="root", PASSWORD="root"):
-        self.connect_to_database(HOST, DATABASE, USER, PASSWORD)
-        self.labels = self.read_labels()
-
-    def connect_to_database(self, HOST, DATABASE, USER, PASSWORD):
+    def __init__(self,
+                 HOST="localhost",
+                 DATABASE="strava_db",
+                 USER="root",
+                 PASSWORD="sveinung"):
         try:
             self.connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=3306)
-            self.cursor = self.connection.cursor()
-            print(f"Established connection to database: {self.connection.get_server_info()}")
         except Exception as e:
-            print(f"ERROR: Failed to connect to db: {e}")
-
-    def read_labels(self):
-        return open(self.DATASET_PATH + "labeled_ids.txt", "r").read().split("\n")
-
-    def create_tables(self):
-        self.create_user_table()
-        self.create_activity_table()
-        self.create_trackpoint_table()
+            print("ERROR: Failed to connect to db:", e)
+        
+        self.cursor = self.connection.cursor()
+        self.labels = open(self.DATASET_PATH + "labeled_ids.txt", "r").read().split("\n") #tells which users have labeled activities
 
     def create_user_table(self):
-        self.execute_query("""
-            CREATE TABLE IF NOT EXISTS user (
+        """
+        Creates the user table if it does not excist
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS User (
                 id VARCHAR(255) PRIMARY KEY,
                 has_labels BOOLEAN
-            );
+            )
         """)
+        self.connection.commit()
 
     def create_activity_table(self):
-        self.execute_query("""
-            CREATE TABLE IF NOT EXISTS activity (
+        """
+        Creates the activity table if it does not excist
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Activity (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 user_id VARCHAR(255),
                 transportation_mode VARCHAR(255),
                 start_date_time DATETIME,
                 end_date_time DATETIME,
-                FOREIGN KEY (user_id) REFERENCES user(id)
-            );
+                FOREIGN KEY (user_id) REFERENCES User(id)
+            )
         """)
+        self.connection.commit()
 
     def create_trackpoint_table(self):
-        self.execute_query("""
-            CREATE TABLE IF NOT EXISTS trackpoint (
+        """
+        Creates the trackpoint table if it does not excist
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS TrackPoint (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 activity_id INT,
                 lat DOUBLE,
                 lon DOUBLE,
                 altitude INT,
                 date DATETIME,
-                FOREIGN KEY (activity_id) REFERENCES activity(id)
-            );
+                FOREIGN KEY (activity_id) REFERENCES Activity(id)
+            )
         """)
-
-    def execute_query(self, query):
-        self.cursor.execute(query)
         self.connection.commit()
 
-    def insert_users(self):
-        user_ids = self.get_user_ids()
+
+    
+    def insert_user_table(self):
+        """
+        This method insert users into the user tables based on the
+        listed user ids in the data directory
+        """
+        user_ids = os.listdir(self.DATASET_PATH + "Data")
+        query = """INSERT INTO User(id,has_labels) VALUES (%s,%s)"""
         for user_id in user_ids:
-            self.insert_single_user(user_id)
-
-    def insert_single_user(self, user_id):
-        query = "INSERT INTO user(id, has_labels) VALUES (%s, %s)"
-        self.cursor.execute(query, (user_id, user_id in self.labels))
+            #self.cursor.execute(query , (user_id,user_id in self.labels))
+            self.cursor.execute(query , (user_id,(user_id in self.labels)))
         self.connection.commit()
 
-    def get_user_ids(self):
-        return sorted(os.listdir(self.DATASET_PATH + "Data"))
+    def insert_activity_table(self,user_id,transportation_mode,start_date_time,end_date_time):
+        """
+        This method insert the given paramters to the activity table
 
-    def insert_activity_table(self, user_id, transportation_mode, start_date_time, end_date_time):
-        query = """INSERT INTO activity(user_id, transportation_mode, start_date_time, end_date_time) VALUES (%s,%s,%s,%s)"""
-        self.cursor.execute(query, (user_id, transportation_mode, start_date_time, end_date_time))
+        :param user_id: The user id for the person doing the activity
+        :param transportation_mode: The type of transportation mode used for this activity
+        :param start_date_time: Activity start time
+        :param end_date_time: Activity end time
+
+        :return: Returns a generated activity id that can be used in trackpoint reference
+        """
+        query = """INSERT INTO Activity(user_id, transportation_mode, start_date_time, end_date_time) VALUES (%s,%s,%s,%s)"""
+        self.cursor.execute(query, (user_id,transportation_mode,start_date_time,end_date_time))
         self.connection.commit()
         return self.cursor.lastrowid
 
-    def insert_trackpoints(self, trackpoints):
-        query = """INSERT INTO trackpoint (activity_id, lat, lon, altitude, date) VALUES (%s,%s,%s,%s,%s)"""
-        self.cursor.executemany(query, trackpoints)
-        self.connection.commit()
+    def insert_trackpoints(self,filtered_trackpoints):
+        """
+        This method insert a bulck of filtered trackpoints to the trackpoint table
 
-    def filter_trackpoints(self, trackpoints, activity_id):
+        :param filtered_trackpoints: List of trackpoints where we have removed unrelevant info and added activity id
+        """
+        query = """INSERT INTO Trackpoint (activity_id, lat, lon, altitude, date) VALUES (%s,%s,%s,%s,%s)"""
+        self.cursor.executemany(query,filtered_trackpoints)
+
+    def filter_trackpoints(self,trackpoints,activity_id):
+        """
+        This method filteres a bunch of trackpoits to include only relevant info and an activity id
+
+        :param trackpoints: trackpoints used to track each user by position, elevation and time
+        :param activity_id: The id for the activity the trackpoint corresponds to
+
+        :return: List of trackpoints where we have removed unrelevant info and added activity id
+        """
         filtered_trackpoints = []
         for trackpoint in trackpoints:
             formated_date = datetime.strptime(trackpoint[5] + trackpoint[6], "%Y-%m-%d%H:%M:%S")
@@ -95,56 +119,75 @@ class DbMaker:
             filtered_trackpoints.append(filtered_trackpoint)
         return filtered_trackpoints
 
-    def find_activity_type(self, id, start_time, end_time):
+    def find_activity_type(self,id,start_time,end_time):
+        """
+        This method filteres a bunch of trackpoits to include only relevant info and an activity id
+
+        :param trackpoints: trackpoints used to track each user by position, elevation and time
+        :param activity_id: The id for the activity the trackpoint corresponds to
+
+        :return: List of trackpoints where we have removed unrelevant info and added activity id
+        """
         user_labels = open(self.DATASET_PATH + "Data/" + id + "/labels.txt").read().split("\n")
         for label in user_labels[1:-1]:
             attribute = label.split("\t")
-            label_start_time = datetime.strptime(attribute[0], "%Y/%m/%d %H:%M:%S")
-            label_end_time = datetime.strptime(attribute[1], "%Y/%m/%d %H:%M:%S")
+            label_start_time = datetime.strptime(attribute[0],"%Y/%m/%d %H:%M:%S")
+            label_end_time = datetime.strptime(attribute[1],"%Y/%m/%d %H:%M:%S")
             if start_time == label_start_time and end_time == label_end_time: return attribute[2]
-        return "NULL"
+        return None
 
     def filter_and_insert_activity(self):
-        self.cursor.execute(""" SELECT * FROM user""")
+        """
+        This method filteres and insert activites and trackpoints to the table
+        """
+        self.cursor.execute(""" SELECT * FROM USER""")
         users_tuple = self.cursor.fetchall()
         for id, is_labeled in users_tuple:
             activity_filenames = os.listdir(self.DATASET_PATH + "Data/" + id + "/Trajectory")
-            if len(activity_filenames) == 0: raise TypeError("No activity files found for user", id)
+            if len(activity_filenames) == 0: raise TypeError("No activity files found for user",id)
             user_data = []
             for activity_filename in activity_filenames:
                 activity_data = read_plt(self.DATASET_PATH + "Data/" + id + "/Trajectory/" + activity_filename)
-                if activity_data == None:
-                    continue
-                start_time_activity = datetime.strptime(activity_data[0][5] + " " + activity_data[0][6],
-                                                        "%Y-%m-%d %H:%M:%S")
-                end_time_activity = datetime.strptime(activity_data[-1][5] + " " + activity_data[-1][6],
-                                                      "%Y-%m-%d %H:%M:%S")
-                activity_type = "NULL"
+                if activity_data == None: continue
+                start_time_activity = datetime.strptime(activity_data[0][5] + " " + activity_data[0][6],"%Y-%m-%d %H:%M:%S")
+                end_time_activity =  datetime.strptime(activity_data[-1][5] + " " + activity_data[-1][6],"%Y-%m-%d %H:%M:%S")
+                activity_type = None
                 if id in self.labels:
-                    activity_type = self.find_activity_type(id, start_time_activity, end_time_activity)
-                activity_id = self.insert_activity_table(id, activity_type, start_time_activity, end_time_activity)
-                filtered_trackpoints = self.filter_trackpoints(activity_data, activity_id)
+                    activity_type = self.find_activity_type(id,start_time_activity,end_time_activity)
+                activity_id = self.insert_activity_table(id,activity_type,start_time_activity,end_time_activity)
+                filtered_trackpoints = self.filter_trackpoints(activity_data,activity_id)
                 self.insert_trackpoints(filtered_trackpoints)
-
-    def close_connection(self):
-        self.connection.close()
-
-
+            
+            
+        
+                
+            
 def read_plt(file_path):
+    """
+    Reads the .plt file and formats it as the spesification in the task
+
+    :param file_path: the .plt file path
+    """
     with open(file_path, 'r') as file:
         lines = file.readlines()
-    if len(lines) > 2500 - 6:
-        return None
+    if len(lines) > 2500 - 6: return None
     data = []
     for line in lines:
         data.append(line.strip().split(','))
     return data[6:]
 
 
-db = DbMaker()
-if db.connection:
-    db.create_tables()
-    db.insert_users()
-    db.filter_and_insert_activity()
-    db.connection.close()
-    print("DbMaker has finished executing.")
+
+    
+
+program = DbMaker()
+program.create_user_table()
+program.create_activity_table()
+program.create_trackpoint_table()
+program.insert_user_table()
+program.filter_and_insert_activity()
+program.connection.close()
+
+
+
+
